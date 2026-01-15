4faaad77907a09f648419d8e72a5f49718f91b4b
Mohammad Yusuf Ali <59096473+Yukii-Codes@users.noreply.github.com>
2026-01-15 14:30:05 +0530

    Add click event to increment count and display it

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
+let count = 0;
 
+const countText = document.getElementById("count");
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+    count++;
+    countText.textContent = count;
+});
